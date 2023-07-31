5729dd4655f78462a90c595aa56fa3f80d44330e
Thomas Martin <tmartin@krakensubsea.com>
2023-07-31 11:58:22 +0200

    Update config.js

--- .tina/config.js
+++ .tina/config.js
@@ -32,6 +32,11 @@ export default defineConfig({
             name: "title",
             label: "title",
           },
+		  {
+            type: "string",
+            name: "youtube",
+            label: "youtube",
+          },
           {
             type: "rich-text",
             name: "body",
